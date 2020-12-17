a3ad7318f6f4db55b679df35240522e0d3e2bdc0
Johnny Liao <32285903+johnnyzzzzzZ@users.noreply.github.com>
2020-12-17 14:03:50 +0800

    Update test_navigation.sh

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-xterm  -e  " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm  -e  " source /opt/ros/kinetic/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 10
-xterm  -e  " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm  -e  " source /opt/ros/kinetic/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 30
-xterm  -e  " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers  view_navigation.launch"
+xterm  -e  " source /opt/ros/kinetic/setup.bash; roslaunch turtlebot_rviz_launchers  view_navigation.launch"
