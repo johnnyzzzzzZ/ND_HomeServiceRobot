53defc5d056d465748c5070c95358c6b53c5c7d9
j <goo543210@gmail.com>
2020-12-18 13:50:53 +0000

    update sh

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/ND_HomeServiceRobot/maps/myworld2.world" &
 sleep 10
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/src/ND_HomeServiceRobot/maps/myMMap.yaml" &
 sleep 30
 xterm  -e  " source devel/setup.bash; roslaunch turtlebot_rviz_launchers  view_navigation.launch" &
 sleep 20
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/ND_HomeServiceRobot/maps/myworld2.world" &
 sleep 20
 xterm  -e  " source devel/setup.bash; rosrun gmapping slam_gmapping" &
 sleep 10
