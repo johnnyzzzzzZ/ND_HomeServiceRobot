089f055a2f6b476333886f10c237e9a03437e1dd
Johnny Liao <32285903+johnnyzzzzzZ@users.noreply.github.com>
2020-12-21 21:34:58 +0900

    Update add_markers.sh

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm  -e  " source devel/setup.sh; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find add_markers)/../maps/myworld2.world" &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find add_markers)/../maps/myworld2.world" &
 sleep 10
-xterm  -e  " source devel/setup.sh; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find add_markers)/../maps/myMMap.yaml" &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find add_markers)/../maps/myMMap.yaml" &
 sleep 10
-xterm  -e  " source devel/setup.sh; roslaunch turtlebot_rviz_launchers  view_navigation.launch" &
+xterm  -e  " roslaunch turtlebot_rviz_launchers  view_navigation.launch" &
 sleep 20
-xterm  -e  " source devel/setup.sh; rosrun add_markers show" 
+xterm  -e  " rosrun add_markers show" 
