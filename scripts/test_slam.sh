2ad04c949f1da097310247aeab2f3e040aaacf68
Johnny Liao <32285903+johnnyzzzzzZ@users.noreply.github.com>
2020-12-21 21:37:00 +0900

    Update test_slam.sh

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find add_markers)/../maps/myworld2.world" &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find add_markers)/../maps/myworld2.world" &
 sleep 20
-xterm  -e  " source devel/setup.bash; rosrun gmapping slam_gmapping" &
+xterm  -e  " rosrun gmapping slam_gmapping" &
 sleep 10
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_rviz_launchers  view_navigation.launch" &
+xterm  -e  " roslaunch turtlebot_rviz_launchers  view_navigation.launch" &
 sleep 10
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
+xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch"
