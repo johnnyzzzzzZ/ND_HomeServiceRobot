37bfcc493d7b20688bc2f379d6147a332d365b20
Johnny Liao <32285903+johnnyzzzzzZ@users.noreply.github.com>
2020-12-21 21:36:10 +0900

    Update pick_objects.sh

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find add_markers)/../maps/myworld2.world" &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find add_markers)/../maps/myworld2.world" &
 sleep 10
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find add_markers)/../maps/myMMap.yaml" &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find add_markers)/../maps/myMMap.yaml" &
 sleep 30
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_rviz_launchers  view_navigation.launch" &
+xterm  -e  " roslaunch turtlebot_rviz_launchers  view_navigation.launch" &
 sleep 20
-xterm  -e  " source devel/setup.bash; rosrun pick_objects mark"
+xterm  -e  " rosrun pick_objects mark"
